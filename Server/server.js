fd5ad9db5dfcc09e2d4a8b4f45bbe60bc6ee0c48
kan32 <kumaraguruparan.a@sky.uk>
2024-06-09 23:30:51 +0530

    Modified api

--- Server/server.js
+++ Server/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.get("/kumara", (req, res) => {
+app.get("/api", (req, res) => {
     res.json({"users": ["userOne", "userTwo", "userThree", "userFour"] })
 })
 
